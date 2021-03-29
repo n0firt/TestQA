d232aea17884024a496425f6d75f95476f1de1c2
n0firt <1botlek@gmail.com>
2021-03-29 19:42:33 +0300

    done

---
+++ index.js
@@ -0,0 +1,110 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+const assert = require('assert');
+
+//buikd webdriver
+let driver = new Builder().forBrowser('chrome').build();
+
+//main func
+(async () => {
+    try{
+        //go to yandex
+        await (await driver).get('https://yandex.ru');
+
+        //wait for page load
+        await waitForPageLoad();
+
+        //check the title
+        await assert.strictEqual('Яндекс', await driver.getTitle());
+
+        //search your words
+        await driver.wait(until.elementLocated(By.name('text')), 5000).sendKeys('расчет расстояний между городами', Key.ENTER);
+
+        //click avtodispetcher site link
+        await driver.wait(until.elementLocated(By.css('a[href="https://www.avtodispetcher.ru/distance/"]')), 5000).click();
+
+        //get all tabs(count would be 2)
+        let windows = await driver.getAllWindowHandles();
+
+        //switch to second tab
+        await driver.switchTo().window(await windows[1]);
+
+        //wait for page load
+        await waitForPageLoad();
+
+        //check the title
+        await assert.strictEqual('Расчет расстояний между городами', await driver.getTitle());
+
+        //fill the form
+        await driver.findElement(By.name('from')).sendKeys('Тула');
+        //
+        await driver.findElement(By.name('to')).sendKeys('Санкт-Петербург');
+        //
+        let fc = await driver.findElement(By.name('fc'));
+        await fc.click();
+        await fc.sendKeys(Key.BACK_SPACE, 9);
+        //
+        let fp = await driver.findElement(By.name('fp'));
+        await fp.click();
+        await fp.sendKeys(Key.BACK_SPACE, Key.BACK_SPACE, 46);
+        //fill the form end
+
+        //submit the form
+        await driver.findElement(By.css('input[type="submit"][value="Рассчитать"]')).click();
+
+        //wait for page load
+        await waitForPageLoad();
+
+        //check the title
+        await assert.strictEqual('Расстояние между Тулой и Санкт-Петербургом', await driver.getTitle());
+
+        //check results
+        await checkResults('897', '3726');
+
+        //click on 'change the way' button
+        await driver.findElement(By.css('span.anchor')).click();
+
+        //fill the input
+        let v = await driver.wait(until.elementLocated(By.name('v')), 5000);
+        await driver.wait(until.elementIsVisible(v), 3000).sendKeys('Великий Новгород');
+        
+        //wait for 30 secs
+        await driver.sleep(30000);                                                                                          //???
+
+        //scroll to the submit button, because it may be hidden and click on it
+        let submitButton2 = await driver.findElement(By.css('input[type="submit"][value="Рассчитать"]'));
+        await driver.executeScript("arguments[0].scrollIntoView()", submitButton2);
+        await driver.sleep(300);
+        await submitButton2.click();
+
+        //check new results
+        await checkResults('966', '4002');
+
+        //log that this test was completely passed
+        await console.log('\x1b[32m'); 
+        await console.log('Passed');
+    }
+    catch(error){
+        //an error happened, so log it with its text
+        await console.log('\x1b[31m', 'Failed\r\n' + 'Error: ' + error.message);
+    }
+    finally{
+        //close the webdriver
+        await driver.quit();
+    }
+})();
+
+//func which waits for the page's loading ending by executing script
+let waitForPageLoad = async () => 
+    await driver.wait(async () => driver.executeScript('return document.readyState')
+    .then(readyState => readyState === 'complete'));
+
+//func which checks results by searching elements and asserting values
+let checkResults = async (result1, result2) => {
+
+    let element1 = await driver.wait(until.elementLocated(By.css('span#totalDistance')), 5000);
+    await assert.strictEqual(result1, await driver.wait(until.elementIsVisible(element1), 5000).getText());
+
+    let element2 = await driver.wait(until.elementLocated(By.css('.distance > #test_summary_variant_d > #summaryContainer > form > p')), 5000);
+    let text = await driver.wait(until.elementIsVisible(element2), 5000).getText();
+    await assert.strictEqual(true, await text.includes(result2));
+};
